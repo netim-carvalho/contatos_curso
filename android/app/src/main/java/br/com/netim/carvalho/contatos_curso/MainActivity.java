f792de96db90bc1c9068022d0040ea6d828235b1
Netim Carvalho <netim.carvalho13@gmail.com>
2022-03-28 11:08:16 -0300

    Initial commit

---
+++ android/app/src/main/java/br/com/netim/carvalho/contatos_curso/MainActivity.java
@@ -0,0 +1,6 @@
+package br.com.netim.carvalho.contatos_curso;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:contatos_curso/views/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MaterialApp(
+    home: HomePage(),
+  ));
+}
